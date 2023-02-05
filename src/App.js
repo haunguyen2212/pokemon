917ba1bd98759118bf19eb2721b38348af50edfb
haunt <trunghau172837@gmail.com>
2023-02-05 14:33:50 +0700

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Header from './components/Header';
 import ListPokemon from './components/home/ListPokemon';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
       <Header />
-      <div style={{ marginTop: "80px" }}></div>
-      <ListPokemon />
+      <div style={{ margin: "80px 0 30px 0" }}>
+        <ListPokemon />
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer id="footer" className="footer">
+        <div className="footer__copyright">
+          © Copyright <strong><span>HauNT</span></strong>. All Rights Reserved
+        </div>
+        <div className="footer__credits">
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
