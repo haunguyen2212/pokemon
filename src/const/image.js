e4296ce3e4785c06b4482492badc62e794cfd1da
haunt <trunghau172837@gmail.com>
2023-02-06 20:06:43 +0700

    remove unused files

--- src/const/image.js
+++
@@ -1,7 +0,0 @@
-const Images = {
-    Fushigidane: require('../pokemon/0001_Fushigidane.png').default,
-    Fushigisou: require('../pokemon/0002_Fushigisou.png').default,
-    Fushigibana: require('../pokemon/0003_Fushigibana.png').default,
-}
-
-export default Images;
