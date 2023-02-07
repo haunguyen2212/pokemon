5aa7f90f340ff89e118184d4812f15f98875b552
haunt <trunghau172837@gmail.com>
2023-02-07 23:09:40 +0700

    add new pokemon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
 function Header() {
+
+  const handleClick = () => {
+    document.getElementById('nav-toggle').click();
+  }
+  
   return (
     <nav className="header">
         <input type="checkbox" id="nav-toggle" className="hidden" />
         <div className="header__logo">Pokemon</div>
         <div className="header__link">
-            <li className="header__item"><Link to="/pokemon">Trang chủ</Link></li>
-            <li className="header__item"><Link to="/collection">Bộ sưu tập</Link></li>
-            <li className="header__item"><Link to="/game">Trò chơi</Link></li>
-            <li className="header__item"><Link to="/quiz">Đố vui</Link></li>
-            <li className="header__item"><Link to="/posts">Bài viết</Link></li>
-            <li className="header__item"><Link to="/link">Liên kết</Link></li>
-            <li className="header__item"><Link to="/contact">Liên hệ</Link></li>
+            <li className="header__item"><Link to="/pokemon" onClick={() => handleClick()}>Trang chủ</Link></li>
+            <li className="header__item"><Link to="/collection" onClick={() => handleClick()}>Bộ sưu tập</Link></li>
+            <li className="header__item"><Link to="/game" onClick={() => handleClick()}>Trò chơi</Link></li>
+            <li className="header__item"><Link to="/quiz" onClick={() => handleClick()}>Đố vui</Link></li>
+            <li className="header__item"><Link to="/posts" onClick={() => handleClick()}>Bài viết</Link></li>
+            <li className="header__item"><Link to="/link" onClick={() => handleClick()}>Liên kết</Link></li>
+            <li className="header__item"><Link to="/contact" onClick={() => handleClick()}>Liên hệ</Link></li>
         </div>
         <label htmlFor="nav-toggle" className="header__icon-menu">
             <div className="header__icon-line"></div>
